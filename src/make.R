f92334973ca5b8b53f194ca265e436f1e52c1a65
Lisa Zorn <lzorn@bayareametro.gov>
2022-09-15 13:25:19 -0700

    Fix bug, add doc, split annual file by district
    Details:
    - write annual district files (rather than just annual)
    - fix bug in write_annual_to_disk (now write_annual_district_to_disk) meta (was district = as.integer(station))
    - revert holiday definitions to be the same as previously defined so that aggregation matches previous version
    - add some function documentation

--- src/make.R
+++ src/make.R
@@ -1,7 +1,8 @@
 
 # Overhead
 packages_vector <- c("tidyverse",
-                     "chron")
+                     "chron",
+                     "timeDate")
 
 need_to_install <- packages_vector[!(packages_vector %in% installed.packages()[,"Package"])]
 
@@ -11,9 +12,9 @@ for (package in packages_vector){
   library(package, character.only = TRUE)
 }
 
-# Remote I/O
-external_dir <- "../data/external/"
-interim_dir <- "../data/interim/"
+# I/O
+external_dir  <- "M:/Data/Traffic/PeMS/"
+interim_dir   <- "../data/interim/"
 processed_dir <- "../data/processed/"
 
 # use "" for none
@@ -24,9 +25,9 @@ output_hour_filename <- paste0(processed_dir, "pems_hour.RDS")
 output_period_filename <- paste0(processed_dir, "pems_period.RDS")
 
 # Parameters
-year_array <- c(2018, 2019)
-district_array <- c(3, 5, 10)
-month_array <- c(3, 4, 5, 9, 10, 11)
+year_array     <- c(2018)
+district_array <- c(3, 4) # c(3, 5, 10)
+month_array    <- c(3, 4, 5, 9, 10, 11)
 
 # Share of sensor data that must be observed to be included
 MIN_PCT_OBS = 100
@@ -40,59 +41,9 @@ MAX_FLOW_ZERO_PLAUSIBLE = 150.0
 # Minimum number of days observed for estimates to be retained
 MIN_DAYS_OBSERVED = 15
 
-HOUR_COLUMN_NAMES <- c("time_stamp_string",
-                       "station",
-                       "district",
-                       "route",
-                       "direction",
-                       "type",
-                       "length",
-                       "samples",
-                       "pct_obs",
-                       "flow",
-                       "occupancy",
-                       "speed",
-                       "delay_35",
-                       "delay_40",
-                       "delay_45",
-                       "delay_50",
-                       "delay_55",
-                       "delay_60",
-                       "flow_1",
-                       "occ_1",
-                       "speed_1",
-                       "flow_2",
-                       "occ_2",
-                       "speed_2",
-                       "flow_3",
-                       "occ_3",
-                       "speed_3",
-                       "flow_4",
-                       "occ_4",
-                       "speed_4",
-                       "flow_5",
-                       "occ_5",
-                       "speed_5",
-                       "flow_6",
-                       "occ_6",
-                       "speed_6",
-                       "flow_7",
-                       "occ_7",
-                       "speed_7",
-                       "flow_8",
-                       "occ_8",
-                       "speed_8")
-
-HOUR_COLUMN_TYPES <- paste0("c",
-                            "i",
-                            "i",
-                            "i",
-                            "c",
-                            "c",
-                            "d",
-                            "i",
-                            "i",
-                            rep("d", 33))
+# Relevant holidays database
+holiday_list  <- c("USLaborDay", "USMemorialDay", "USThanksgivingDay", "USVeteransDay")
+holiday_dates <- dates(as.character(holiday(2000:2025, holiday_list)), format = "Y-M-D")
 
 # Make Time Periods Map
 time_per_df <- tibble(hour = c(seq(0, 23)), 
@@ -107,40 +58,103 @@ time_per_counts_df <- select(as.data.frame(table(time_per_df$time_period)),
                              time_period = Var1, 
                              time_period_count = Freq)
 
-# Methods
+# Methods ----------------------------------------------------------------------
+
+#' Check for meta data file, read it and return as a dataframe
 consume_meta <- function(data_dir, district, year) {
   
   district_string <- sprintf("%02d", district)
-  meta_file_vector <- list.files(path = data_dir, 
+  meta_file_vector <- list.files(path = file.path(data_dir, year), 
                                  pattern = paste0(district_string, "_text_meta_", year))
   stopifnot(length(meta_file_vector) > 0)
-  meta_filename <- paste0(data_dir, meta_file_vector[1])
+  meta_filename <- file.path(data_dir, year, meta_file_vector[1])
   df <- read_delim(meta_filename, delim = "\t", col_types = cols(.default = col_character()))
-  
+  print(problems(df))
   return(df)
 }
 
+#' Read monthly data files (e.g., d[DD]_text_station_hour_[YYYY]_[MM].txt) and
+#' return as a dataframe
 consume_raw <- function(data_dir, district, year, month){
+  
+  HOUR_COLUMN_NAMES <- c("time_stamp_string",
+                         "station",
+                         "district",
+                         "route",
+                         "direction",
+                         "type",
+                         "length",
+                         "samples",
+                         "pct_obs",
+                         "flow",
+                         "occupancy",
+                         "speed",
+                         "delay_35",
+                         "delay_40",
+                         "delay_45",
+                         "delay_50",
+                         "delay_55",
+                         "delay_60",
+                         "flow_1",
+                         "occ_1",
+                         "speed_1",
+                         "flow_2",
+                         "occ_2",
+                         "speed_2",
+                         "flow_3",
+                         "occ_3",
+                         "speed_3",
+                         "flow_4",
+                         "occ_4",
+                         "speed_4",
+                         "flow_5",
+                         "occ_5",
+                         "speed_5",
+                         "flow_6",
+                         "occ_6",
+                         "speed_6",
+                         "flow_7",
+                         "occ_7",
+                         "speed_7",
+                         "flow_8",
+                         "occ_8",
+                         "speed_8")
+  
+  HOUR_COLUMN_TYPES <- paste0("c", # time_stamp_string
+                              "i", # station
+                              "i", # district
+                              "i", # route
+                              "c", # direction
+                              "c", # type
+                              "d", # length
+                              "i", # samples
+                              "i", # pct_obs
+                              rep("d", 33))
   
   district_string <- sprintf("%02d", district)
   month_string <- sprintf("%02d", month)
   
-  filename <- paste0(data_dir, "d", district_string, "_text_station_hour_", year, "_", month_string, ".txt")
-  df <- read_csv(filename, col_names = HOUR_COLUMN_NAMES, col_types = HOUR_COLUMN_TYPES)
-  
+  filename <- paste0("d", district_string, "_text_station_hour_", year, "_", month_string, ".txt")
+  df <- read_csv(file.path(data_dir, year, filename), col_names = HOUR_COLUMN_NAMES, col_types = HOUR_COLUMN_TYPES)
+  print(problems(df))
   return(df)
-  
 }
 
+#' Converts time_stamp_string to date/your/day_of_week
+#' Filters to rows with pct_obs >= MIN_PCT_OBS
+#' Filters out holidays and Weekends/Monday/Friday
+#' Adds lanes column based on presence of flow data for lanes
 clean_raw <- function(input_df){
   
+  print('clean_raw: input_df head:')
+  print(head(input_df))
   df <- input_df %>%
     filter(pct_obs >= MIN_PCT_OBS) %>%
     mutate(date = as.Date(time_stamp_string, format = "%m/%d/%Y %H:%M:%S")) %>%
     mutate(hour = as.numeric(str_sub(as.character(time_stamp_string), 12, 13))) %>%
     mutate(day_of_week = weekdays(date)) %>%
     filter(day_of_week == "Tuesday" | day_of_week == "Wednesday" | day_of_week == "Thursday") %>%
-    filter(!is.holiday(date)) %>%
+    filter(!is.holiday(date, holiday_dates)) %>%
     mutate(lanes = 8) %>%
     mutate(lanes = ifelse(is.na(flow_8), 7, lanes)) %>%
     mutate(lanes = ifelse(is.na(flow_7), 6, lanes)) %>%
@@ -152,29 +166,30 @@ clean_raw <- function(input_df){
     select(date, day_of_week, station, district, route, direction, type, hour, 
            pct_obs, length, flow, speed, lanes, occupancy)
   
+  print(head(df))
   return(df)
-  
-  
 }
 
+#' Flag some rows as "suspect' if there are flows that exceed MAX_FLOW_ZERO_PLAUSIBLE
+#' when aggregated to station/hour and filter them out
 remove_suspect <- function(input_df, time_period_df){
   
   df <- left_join(input_df, time_per_df, by = c("hour"))
   
   sum_hour_df <- df %>%
     group_by(station, district, route, direction, type, hour, lanes) %>%
-    summarise(median_flow = median(flow),      
-              avg_flow = mean(flow),      
-              sd_flow = sd(flow), 
-              max_flow = max(flow), 
-              min_flow = min(flow), 
+    summarise(median_flow  = median(flow),      
+              avg_flow     = mean(flow),      
+              sd_flow      = sd(flow), 
+              max_flow     = max(flow), 
+              min_flow     = min(flow), 
               median_speed = median(speed),     
-              avg_speed = mean(speed),     
-              sd_speed = sd(speed),
+              avg_speed    = mean(speed),     
+              sd_speed     = sd(speed),
               median_occup = median(occupancy), 
-              avg_occup = mean(occupancy), 
+              avg_occup    = mean(occupancy), 
               sd_occupancy = sd(occupancy),
-              .groups = "drop")
+              .groups      = "drop")
   
   suspect_stations_df <- sum_hour_df %>%
     mutate(suspect_station = ifelse(max_flow > MAX_FLOW_ZERO_PLAUSIBLE & min_flow == 0L, TRUE, FALSE)) %>%
@@ -189,19 +204,22 @@ remove_suspect <- function(input_df, time_period_df){
   
 }
 
+#' Summarize given data frame to (station, district, route, direction, type, hour, lanes),
+#' filtering to only rows with days_observed > MIN_DAYS_OBSERVED
+#' Returns summary data frame
 sum_for_hours <- function(input_df) {
   
   df <- input_df %>%
     group_by(station, district, route, direction, type, hour, lanes) %>%
-    summarise(median_flow = median(flow),      
-              avg_flow = mean(flow),      
-              sd_flow = sd(flow), 
-              median_speed = median(speed),     
-              avg_speed = mean(speed),     
-              sd_speed = sd(speed),
-              median_occup = median(occupancy), 
-              avg_occup = mean(occupancy), 
-              sd_occupancy = sd(occupancy),
+    summarise(median_flow   = median(flow),      
+              avg_flow      = mean(flow),      
+              sd_flow       = sd(flow), 
+              median_speed  = median(speed),     
+              avg_speed     = mean(speed),     
+              sd_speed      = sd(speed),
+              median_occup  = median(occupancy), 
+              avg_occup     = mean(occupancy), 
+              sd_occupancy  = sd(occupancy),
               days_observed = n(),
               .groups = "drop") %>%
     filter(days_observed > MIN_DAYS_OBSERVED)
@@ -210,6 +228,9 @@ sum_for_hours <- function(input_df) {
 
 }
 
+#' Summarize given data frame to (station, district, route, direction, type, hour, lanes),
+#' filtering to only rows with days_observed > MIN_DAYS_OBSERVED
+#' Returns summary data frame
 sum_for_periods <- function(input_df) {
   
   df <- input_df %>%
@@ -243,22 +264,24 @@ sum_for_periods <- function(input_df) {
   
 }
 
-write_annual_to_disk <- function(out_dir, input_hour_df, input_period_df, input_meta_df, input_year_int){
+#' Write out annual files for hourly data and time period data by district
+write_annual_district_to_disk <- function(out_dir, input_hour_df, input_period_df, input_meta_df, 
+                                          input_year_int, input_district_int){
   
   join_meta_df <- input_meta_df %>%
-    select(station = ID, 
-           district = District, 
-           route = Fwy, 
+    select(station   = ID, 
+           district  = District, 
+           route     = Fwy, 
            direction = Dir, 
-           type = Type,
-           state_pm = State_PM, 
-           abs_pm = Abs_PM, 
-           latitude = Latitude, 
+           type      = Type,
+           state_pm  = State_PM, 
+           abs_pm    = Abs_PM, 
+           latitude  = Latitude, 
            longitude = Longitude) %>%
-    mutate(station = as.integer(station),
-           route = as.integer(route),
-           district = as.integer(station),
-           latitude = as.double(latitude),
+    mutate(station   = as.integer(station),
+           route     = as.integer(route),
+           district  = as.integer(district),
+           latitude  = as.double(latitude),
            longitude = as.double(longitude))
   
   write_hour_df <- left_join(input_hour_df, 
@@ -271,11 +294,13 @@ write_annual_to_disk <- function(out_dir, input_hour_df, input_period_df, input_
                                by = c("station","district","route","direction","type")) %>%
     mutate(year = input_year_int)
   
-  output_hour_filename <- paste0(out_dir, "pems_hour_", year, ".RDS")
-  output_period_filename <- paste0(out_dir, "pems_period_", year, ".RDS")
+  output_hour_filename   <- file.path(out_dir, sprintf("pems_hour_d%02d_%d.RDS", input_district_int, input_year_int))
+  output_period_filename <- file.path(out_dir, sprintf("pems_period_d%02d_%d.RDS", input_district_int, input_year_int))
   
   saveRDS(write_hour_df, file = output_hour_filename)
+  print(paste("Wrote",output_hour_filename))
   saveRDS(write_period_df, file = output_period_filename)
+  print(paste("Wrote",output_period_filename))
   
   
 }
@@ -298,7 +323,7 @@ for (year in year_array) {
     
     for (month in month_array){
       
-      print(paste0("Consuming Month ", month, ", Year ", year, " for District ", district))
+      print(paste("Consuming Year", year, "Month", month, "for District", district))
       
       raw_df <- consume_raw(external_dir, district, year, month)
       clean_df <- clean_raw(raw_df)
@@ -312,14 +337,17 @@ for (year in year_array) {
     annual_hourly_sum_df <- sum_for_hours(temp_df)
     annual_period_sum_df <- sum_for_periods(temp_df)
     
+    write_annual_district_to_disk(processed_dir, 
+                                  annual_hourly_sum_df, 
+                                  annual_period_sum_df, meta_df, 
+                                  year, district)
+    
     remove(temp_df)
     
     across_districts_hour_df <- bind_rows(annual_hourly_sum_df, across_districts_hour_df)
     across_districts_period_df <- bind_rows(annual_period_sum_df, across_districts_period_df)
     
   } # district
-  
-  write_annual_to_disk(processed_dir, across_districts_hour_df, across_districts_period_df, meta_df, year)
   
   across_years_hour_df <- bind_rows(across_districts_hour_df, across_years_hour_df)
   across_years_period_df <- bind_rows(across_districts_period_df, across_years_period_df)
